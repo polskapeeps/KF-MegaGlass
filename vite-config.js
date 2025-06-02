126f0b029c0e3e15bbd73340c90b3d7990608b8a
polskapeeps <poitrkostka@gmail.com>
2025-06-02 00:33:09 -0400

    Adds vite configuration file
    Sets up the Vite configuration file.
    
    This allows for using Vite as a build tool for the project,
    enabling faster development and optimized production builds.

---
+++ vite-config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/KF-MegaGlass/', //  IMPORTANT: Replace your-repo-name with your GitHub repository name!
+  build: {
+    outDir: 'dist', // This is the default, but good to be explicit
+  },
+});
